e105b2638fd2c7c8e1202c975a0c10437c541db3
TOMASZPJATK <134141752+TOMASZPJATK@users.noreply.github.com>
2023-05-20 14:00:31 +0200

    Update app1_niekompletne.py
    zmieniłem sex na plec i age na wiek

--- app1_niekompletne.py
+++ app1_niekompletne.py
@@ -28,12 +28,12 @@ def main():
 		st.title("Wyrocznia.exe")
 
 	with left:
-		sex_radio = st.radio( "Płeć", list(sex_d.keys()), format_func=lambda x : sex_d[x] )
+		plec_radio = st.radio( "Płeć", list(sex_d.keys()), format_func=lambda x : sex_d[x] )
 		embarked_radio = st.radio( "Port zaokrętowania", list(embarked_d.keys()), index=2, format_func= lambda x: embarked_d[x] )
 		pclass_radio = st.radio( "Klasa", list(pclass_d.keys()), format_func=lambda x : pclass_d[x] )
 
 	with right:
-		age_slider = st.slider("Wiek", value=1, min_value=1, max_value=80)
+		wiek_slider = st.slider("Wiek", value=1, min_value=1, max_value=80)
 		sibsp_slider = st.slider("Liczba rodzeństwa i/lub partnera", min_value=0, max_value=8)
 		parch_slider = st.slider("Liczba rodziców i/lub dzieci", min_value=0, max_value=6)
 		fare_slider = st.slider("Cena biletu", min_value=0, max_value=513, step=1)
